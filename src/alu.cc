8931b3a458f2040113ec4c0a4e0222f48c617f01
n4th0 <nathanmoyses@gmail.com>
2024-05-31 16:17:09 +0200

    add shifters and merged to alu

--- src/alu.cc
+++ src/alu.cc
@@ -94,34 +94,83 @@ int32_t sumador(int32_t a, int32_t b, bool suma){
     return base;
 }
 
-// TODO pensar como hacer esta funcion con los desplazadores nuevos
+
+void desplazadorBooth(int32_t &up, int32_t &down){
+    bool a;
+    a = (up & 1);
+    up = desplazadorDerecha(up, 1);
+    down = desplazadorDerecha(down, 1); 
+    down = down & 0x7FFFFFFF; // de aritmético a lógico
+
+    if (a) {
+        down = down | 0x80000000;
+    }
+
+}
+
+int64_t ttos(int32_t up, int32_t down){
+    int64_t a = 0;
+    if(up & 0x00000001) { a = a | 0x0000000100000000; }
+    if(up & 0x00000002) { a = a | 0x0000000200000000; }
+    if(up & 0x00000004) { a = a | 0x0000000400000000; }
+    if(up & 0x00000008) { a = a | 0x0000000800000000; }
+    if(up & 0x00000010) { a = a | 0x0000001000000000; }
+    if(up & 0x00000020) { a = a | 0x0000002000000000; }
+    if(up & 0x00000040) { a = a | 0x0000004000000000; }
+    if(up & 0x00000080) { a = a | 0x0000008000000000; }
+    if(up & 0x00000100) { a = a | 0x0000010000000000; }
+    if(up & 0x00000200) { a = a | 0x0000020000000000; }
+    if(up & 0x00000400) { a = a | 0x0000040000000000; }
+    if(up & 0x00000800) { a = a | 0x0000080000000000; }
+    if(up & 0x00001000) { a = a | 0x0000100000000000; }
+    if(up & 0x00002000) { a = a | 0x0000200000000000; }
+    if(up & 0x00004000) { a = a | 0x0000400000000000; }
+    if(up & 0x00008000) { a = a | 0x0000800000000000; }
+    if(up & 0x00010000) { a = a | 0x0001000000000000; }
+    if(up & 0x00020000) { a = a | 0x0002000000000000; }
+    if(up & 0x00040000) { a = a | 0x0004000000000000; }
+    if(up & 0x00080000) { a = a | 0x0008000000000000; }
+    if(up & 0x00100000) { a = a | 0x0010000000000000; }
+    if(up & 0x00200000) { a = a | 0x0020000000000000; }
+    if(up & 0x00400000) { a = a | 0x0040000000000000; }
+    if(up & 0x00800000) { a = a | 0x0080000000000000; }
+    if(up & 0x01000000) { a = a | 0x0100000000000000; }
+    if(up & 0x02000000) { a = a | 0x0200000000000000; }
+    if(up & 0x04000000) { a = a | 0x0400000000000000; }
+    if(up & 0x08000000) { a = a | 0x0800000000000000; }
+    if(up & 0x10000000) { a = a | 0x1000000000000000; }
+    if(up & 0x20000000) { a = a | 0x2000000000000000; }
+    if(up & 0x40000000) { a = a | 0x4000000000000000; }
+    if(up & 0x80000000) { a = a | 0x8000000000000000; }
+    a = a | down;
+    return a;
+}
+
 int64_t booth(int32_t a, int32_t b){
-    int64_t result;
-    int64_t mascaraAbajo = 0x0000000000000001;
-    result = (int64_t)a;
+    int32_t mascaraAbajo = 1;
     bool q = false;
-    int32_t resultadoAlu;
+    int32_t up = 0;
+    int32_t down = b;
 
     for (int i = 0; i<32; i++) {
 
-        if ( (result & mascaraAbajo) && !q) {
+        if ((down & mascaraAbajo) && !q) {
             // resta
-            resultadoAlu = sumador(((int32_t)(result >> 32)),  b, false);
-            result = ((result & 0x00000000FFFFFFFF) | (((int64_t)resultadoAlu) << 32));
-
-        } else if ( !(result & mascaraAbajo) && q) {
+            up = sumador(up,  a, false);
+        } else if (!(down & mascaraAbajo) && q) {
             // suma
-            resultadoAlu = sumador((int32_t)(result >> 32),  b, true);
-            result = ((result & 0x00000000FFFFFFFF) | (((int64_t)resultadoAlu) << 32));
+            up = sumador(up,  a, true);
         }     
 
-        q = (result & mascaraAbajo);
+        q = (down & mascaraAbajo);
 
-        result = result >> 1;
+
+        desplazadorBooth(up, down);
+        // result = result >> 1;
 
     }
 
-    return result;
+    return ttos(up, down);
 }
 
 
